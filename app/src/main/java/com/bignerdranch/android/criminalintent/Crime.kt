9ab5ee078c7b9162983b30f6083cc1fdaf0cc4fd
PaladiRka <milakins@bk.ru>
2021-02-07 07:50:10 +0300

    Crime can be created by UUID.

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -2,8 +2,8 @@ package com.bignerdranch.android.criminalintent
 
 import java.util.*
 
-class Crime(val requiresPolice: Boolean) {
-    val id: UUID = UUID.randomUUID()
+class Crime(val requiresPolice: Boolean, val id: UUID) {
+    constructor(requiresPolice: Boolean) : this(requiresPolice, UUID.randomUUID())
     var date: Date = Date()
     var title: String = ""
     var isSolved: Boolean = false
